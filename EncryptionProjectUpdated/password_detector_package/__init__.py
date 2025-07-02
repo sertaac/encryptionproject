ddbb88b65c92c3bfac5717f9b899603d04ed7c3e
Sertaç <sertacataac@gmail.com>
2025-07-02 10:10:14 +0300

    Update __init__.py

--- EncryptionProjectUpdated/password_detector_package/__init__.py
+++ EncryptionProjectUpdated/password_detector_package/__init__.py
@@ -1,6 +1,10 @@
-# EncryptionProject/password_detector_package/__init__.py
+# EncryptionProject/password_detector_package/__init__.py #
+
+# Import main classes or functions exposed by the package here. #
+# This allows users to access these components directly when importing the package. #
 
 from .detector import PasswordProtectionDetector
 from .sync_detector import SynchronousPasswordProtectionDetector # Yeni import
 
+# Optionally, define __all__ to control what's imported by `from package import *` #
 __all__ = ['PasswordProtectionDetector', 'SynchronousPasswordProtectionDetector']
